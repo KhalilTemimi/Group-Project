c6d981b910724c168a69ea651ced932692b716de
YahiaJouini <jouiniyahya117@gmail.com>
2024-02-06 17:21:15 +0100

    adding an update function

--- Devs on Deck/server/routes/developer.route.js
+++ Devs on Deck/server/routes/developer.route.js
@@ -2,5 +2,6 @@ const devControlelr = require('../controllers/developer.controller')
 module.exports = (app) => {
     app.post('/api/register', devControlelr.register)
     app.post('/api/login', devControlelr.login)
-    app.get('/api/getDev',devControlelr.findDev)
+    app.get('/api/getDev', devControlelr.findDev)
+    app.post('/api/addSkill/:id', devControlelr.addSkills)
 }
